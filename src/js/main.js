e402b0835e50c524b52652d9b34d2de8fe6d1eb1
MateuszChmiest <mateusz_269@icloud.com>
2022-08-20 12:29:53 +0200

    local storage and reset button

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,38 @@
-const popup = document.querySelector('.popup')
-const button = document.getElementById('counterButton');
-const close = document.getElementById('close');
-const popupLabel = document.getElementById('popup-label');
-const reset = document.getElementById('resetButton');
+const popup = document.querySelector(".popup");
+const button = document.getElementById("counterButton");
+const close = document.getElementById("close");
+const popupLabel = document.getElementById("popup-label");
+const reset = document.getElementById("resetButton");
+const counter = document.getElementById("counter");
 
 const handleToggle = () => {
-    popup.classList.toggle('active');
-    popupLabel.classList.toggle('active-label')
-}
+	popup.classList.toggle("active");
+	popupLabel.classList.toggle("active-label");
+};
 
 const handleClose = (event) => {
-    if (event.target.closest('#popup-label')) {
-        return;
-    }
-    handleToggle()
-}
+	if (event.target.closest("#popup-label")) {
+		return;
+	}
+	handleToggle();
+};
 
-button.addEventListener('click', handleToggle);
-close.addEventListener('click', handleToggle);
-popup.addEventListener('click', handleClose);
+let count = localStorage.getItem('count') || 0;
+
+button.addEventListener("click", () => {
+    handleToggle();
+	count++;
+    localStorage.setItem('count', count);
+    counter.innerText = `${count} times`;
+    count >= 5 ? reset.classList.add("active") : reset.classList.remove("active")
+});
+
+
+
+reset.addEventListener("click", () => {
+	localStorage.clear()
+    count = 0
+    counter.innerText = `${count} times`;
+});
+close.addEventListener("click", handleToggle);
+popup.addEventListener("click", handleClose);
